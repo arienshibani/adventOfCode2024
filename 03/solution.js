b75263fb68bd7bfd4335f6d2a7031963d807fd00
Arien Shibani <aaari94@gmail.com>
2024-12-03 16:31:17 +0100

    feat(03): :dizzy: Solved 3.dec!

--- 03/solution.js
+++ 03/solution.js
@@ -5,18 +5,12 @@ const file = fs.readFileSync("./input.txt", "utf8");
 const mulFunctions = /(mul\(([0-9]*),([0-9]*)\))/g
 const found = file.match(mulFunctions)
 
-
 const parseMulFunction = (mulFunction) => {
-    const params = mulFunction.match(/([0-9]*)/g)
+    const params = mulFunction.match(/([0-9]*)/g).filter(el => el.length !== 0)
     return params[0] * params[1]
-
 }
 
-const result = found.map(el => parseMulFunction(el))
+const listOfMultiplications = found.map(el => { return parseMulFunction(el) });
+const sum = listOfMultiplications.reduce((acc, curr) => acc + curr, 0);
 
-let sum;
-for (const number in result) {
-    sum += number
-}
-
-console.log(sum);
+console.log("The total sum of all the multiplications are:", sum);

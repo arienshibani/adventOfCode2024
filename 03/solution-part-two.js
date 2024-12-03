9b8d8d3596b9f00e4783f723927141a836324516
Arien Shibani <aaari94@gmail.com>
2024-12-03 17:03:38 +0100

    feat(03): :dizzy: Solved 3.December, part 2!

--- 03/solution-part-two.js
+++ 03/solution-part-two.js
@@ -2,22 +2,41 @@
 var fs = require("fs");
 const file = fs.readFileSync("./input.txt", "utf8");
 
-const mulFunctions = /(mul\(([0-9]*),([0-9]*)\))/g
-const found = file.match(mulFunctions)
+// Use regular expressions to match all non-corrupt mul(), do() and don't() functions from the instruction set.
+const allValidInstructions = /(mul\(([0-9]*),([0-9]*)\))|(do\(\)|don't\(\))/g
+const found = file.match(allValidInstructions)
+
+
+// Controls's wether or not the mul() functions should do anything.
+let isMulEnabled = true; // Defaults to true
 
 /**
- *  Use regular expressions to match all non-corrupt mul() functions from the instruction set.
+ *  Use regular expressions to parse the parameters of a mul() function, and do the math.
  *
  * @param {string} mulFunction
- * @returns {number}
+ * @returns {number} The multiplied value of the params.
  */
 const parseMulFunction = (mulFunction) => {
     const params = mulFunction.match(/([0-9]*)/g).filter(el => el.length !== 0)
-    return params[0] * params[1]
+    if (!params.length) {
+        return 0
+    }
+    return Number.parseInt(params[0]) * Number.parseInt(params[1])
 }
 
-// Parse the list of regex matches, and add them all up.
-const listOfMultiplications = found.map(el => { return parseMulFunction(el) });
-const sum = listOfMultiplications.reduce((acc, curr) => acc + curr, 0);
+let sum = 0;
+for (const instruction of found) {
+    if (instruction === "don't()") {
+        isMulEnabled = false
+    }
+
+    if (instruction === "do()") {
+        isMulEnabled = true
+    }
+
+    if (isMulEnabled) {
+        sum += parseMulFunction(instruction)
+    }
+}
 
 console.log("The sum of all multiplications are:", sum);
--- 03/solution.js
+++ 03/solution.js
@@ -2,14 +2,15 @@
 var fs = require("fs");
 const file = fs.readFileSync("./input.txt", "utf8");
 
+// Use regular expressions to match all non-corrupt mul() functions from the instruction set.
 const mulFunctions = /(mul\(([0-9]*),([0-9]*)\))/g
 const found = file.match(mulFunctions)
 
 /**
- *  Use regular expressions to match all non-corrupt mul() functions from the instruction set.
+ *  Use regular expressions to retreive the actual numbers from the raw instructions, and multiply them.
  *
  * @param {string} mulFunction
- * @returns {number}
+ * @returns {number} The multiplied value of the params.
  */
 const parseMulFunction = (mulFunction) => {
     const params = mulFunction.match(/([0-9]*)/g).filter(el => el.length !== 0)
